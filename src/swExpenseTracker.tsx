26b853c1aa0a186ffdede93e9b157966df8fb68e
Arsalan Raza Azmi <arsalan.azmi92@gmail.com>
2020-09-20 17:17:22 +0500

    Text Aligned

--- src/swExpenseTracker.tsx
+++ src/swExpenseTracker.tsx
@@ -1,13 +1,13 @@
 export const register = () => {
-    
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/sw.js')
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js')
       .then(registration => {
-        
+
         console.log(`Service Worker registration complete, scope: '${registration.scope}'`)
-       })
+      })
       .catch(error => {
         console.log('Error registering service worker! Error is:', error);
-       });
-    }
-  };
+      });
+  }
+};
